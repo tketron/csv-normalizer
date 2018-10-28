10ff4e72708896c9d83df639d33d2a644d16f983
Tyler Ketron <tketron@gmail.com>
2018-10-28 14:05:03 -0700

    added function to map column numbers to names

--- normalizer.js
+++ normalizer.js
@@ -1,19 +1,27 @@
 function normalizer(data) {
   let result = [];
-  for (let i = 0; i < data.length; i++) {
-    let row = data[i];
-    if (i > 0) {
-      row = normalizeRow(row);
-    }
+  result.push(data[0]);
+
+  const columnMap = constructColumnMap(data[0]);
+  console.log(columnMap);
+  for (let i = 1; i < data.length; i++) {
+    const row = normalizeRow(data[i], columnMap);
     console.log(row);
     result.push(row);
   }
-  // convert result back to csv
   return result;
 }
 
-function normalizeRow(row) {
+function normalizeRow(row, columnMap) {
   return row;
+}
+
+function constructColumnMap(columns) {
+  let columnMap = {};
+  for (let i = 0; i < columns.length; i++) {
+    columnMap[i] = columns[i];
+  }
+  return columnMap;
 }
 
 module.exports = normalizer;
