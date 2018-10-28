86101808956d4113af0bda0e48b2e787cea3960e
Tyler Ketron <tketron@gmail.com>
2018-10-28 14:19:10 -0700

    scaffolded normalize functions

--- normalizer.js
+++ normalizer.js
@@ -13,7 +13,16 @@ function normalizer(data) {
 }
 
 function normalizeRow(row, columnMap) {
-  return row;
+  let normalizedRow = Array.from(row);
+  normalizedRow[0] = normalizeTimestamp(row[0]);
+  normalizedRow[1] = normalizeAddress(row[1]);
+  normalizedRow[2] = normalizeZip(row[2]);
+  normalizedRow[3] = normalizeName(row[3]);
+  normalizedRow[4] = normalizeFooBar(row[4]);
+  normalizedRow[5] = normalizeFooBar(row[5]);
+  normalizedRow[6] = normalizeTotal(normalizedRow[4], normalizedRow[5]);
+  normalizedRow[7] = normalizeNotes(row[7]);
+  return normalizedRow;
 }
 
 function constructColumnMap(columns) {
@@ -21,7 +30,43 @@ function constructColumnMap(columns) {
   for (let i = 0; i < columns.length; i++) {
     columnMap[i] = columns[i];
   }
+
+  // for (let key of columnMap) {
+  //   if (typeof columnMap[key] === 'string') {
+  //     columnMap[key] = getTransformFunction()
+  //     //get appropriate function
+  //   }
+  // }
+
   return columnMap;
+}
+
+function normalizeTimestamp(timestamp) {
+  return timestamp;
+}
+
+function normalizeAddress(address) {
+  return address;
+}
+
+function normalizeZip(zip) {
+  return zip;
+}
+
+function normalizeName(name) {
+  return name;
+}
+
+function normalizeFooBar(time) {
+  return time;
+}
+
+function normalizeTotal(foo, bar) {
+  return foo + bar;
+}
+
+function normalizeNotes(notes) {
+  return notes;
 }
 
 module.exports = normalizer;
